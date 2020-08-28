c1b9e3cc4599414804f1164f9f461b37030009ed
kakspark <anilkanaparthi1985@gmail.com>
2020-08-28 18:15:41 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -3,4 +3,5 @@ print("Hello team under devops we are learning GIT")
 print("today i am using git basic commands")
 print("changes are made in master repos")
 print("i created new project called DemoDevOps")
+print("user-2 modified the code in global master repos")
 
