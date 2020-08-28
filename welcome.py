f43400d3ec3b25b46616f88e138cb5afe543b286
Anil Kumar Kanaparthi (akanaparthi) <akanaparthi@micron.com>
2020-08-28 18:10:40 +0530

    New code changes done by user-1

--- welcome.py
+++ welcome.py
@@ -2,3 +2,5 @@ print("Hello India Team")
 print("Hello team under devops we are learning GIT")
 print("today i am using git basic commands")
 print("changes are made in master repos")
+print("i created new project called DemoDevOps")
+
