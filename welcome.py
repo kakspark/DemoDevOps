cbeffd4fc11823441fc1186369eafc97799a0fe3
Anil Kumar Kanaparthi (akanaparthi) <akanaparthi@micron.com>
2020-08-29 16:58:51 +0530

    Feature1 completed

--- welcome.py
+++ welcome.py
@@ -4,4 +4,5 @@ print("today i am using git basic commands")
 print("changes are made in master repos")
 print("i created new project called DemoDevOps")
 print("user-2 modified the code in global master repos")
+print("This is feature1 change")
 
