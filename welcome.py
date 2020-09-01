024729dcdff1b20bc1f4d45a5acfb464d259c349
user2 <akanaparthi@micron.com>
2020-09-01 17:49:32 +0530

    cherry-pick

--- welcome.py
+++ welcome.py
@@ -10,4 +10,8 @@ print("welcome to user-2 ... edited by user-1")
 print("changes are made in f1 branch")
 print("fast forward merge")
 print("changes made by local f1 branch")
+<<<<<<< HEAD
 print("welcome to user-2 ... edited by user-1")
+=======
+print("welcome to user-2... title edited by user-2 only")
+>>>>>>> ae6990b... user-2 edited title
