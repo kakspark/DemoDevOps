3bc0df6900e6aa3f486136ace6dd0a295262ba20
user2 <akanaparthi@micron.com>
2020-09-01 17:49:32 +0530

    cherry pick by user-2

--- welcome.py
+++ welcome.py
@@ -10,5 +10,12 @@ print("welcome to user-2 ... edited by user-1")
 print("changes are made in f1 branch")
 print("fast forward merge")
 print("changes made by local f1 branch")
+<<<<<<< HEAD
 print("welcome to user-2 ... edited by user-1")
+<<<<<<< HEAD
 print("rebase example by user-1")
+=======
+=======
+print("welcome to user-2... title edited by user-2 only")
+>>>>>>> ae6990b... user-2 edited title
+>>>>>>> 024729d... cherry-pick
