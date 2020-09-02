3d4eb3689d660047d4e935fb1582b9d0ba23ea7e
user1 <akanaparthi@micron.com>
2020-09-02 17:23:43 +0530

    rebase example user-1

--- welcome.py
+++ welcome.py
@@ -10,8 +10,5 @@ print("welcome to user-2 ... edited by user-1")
 print("changes are made in f1 branch")
 print("fast forward merge")
 print("changes made by local f1 branch")
-<<<<<<< HEAD
 print("welcome to user-2 ... edited by user-1")
-=======
-print("welcome to user-2... title edited by user-2 only")
->>>>>>> ae6990b... user-2 edited title
+print("rebase example by user-1")
